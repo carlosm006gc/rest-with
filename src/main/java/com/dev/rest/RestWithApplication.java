5794022398e253e4a8955870728f2eadaa7ccd24
carlos006gc <carlos.65613@gmail.com>
2024-02-07 16:05:09 -0300

    first commit

---
+++ src/main/java/com/dev/rest/RestWithApplication.java
@@ -0,0 +1,13 @@
+package com.dev.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithApplication.class, args);
+	}
+
+}
